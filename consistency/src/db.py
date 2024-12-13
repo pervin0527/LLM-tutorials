d6150f1d98f3cd342239de2a574d951455fe9442
pervin0527 <zfbtldnz@gmail.com>
2024-12-13 07:58:21 +0000

    Updated 2024.12.13

---
+++ consistency/src/db.py
@@ -0,0 +1,10 @@
+import psycopg2
+import requests
+
+db_config = {
+    'host': 'your_database_host',
+    'database': 'your_database_name',
+    'user': 'your_username',
+    'password': 'your_password',
+    'port': 5432  # 포트 번호
+}
